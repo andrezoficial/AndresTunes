f522383b880b2d26cf28c4573f37c7246806655b
Andres suarez moreno <114126047+andrezoficial@users.noreply.github.com>
2025-06-07 17:06:14 -0500

    Update lyrics.js

--- src/utils/lyrics.js
+++ src/utils/lyrics.js
@@ -1,1 +1,9 @@
-
+export async function getLyrics(artist, title) {
+  try {
+    const response = await fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`);
+    const data = await response.json();
+    return data.lyrics || 'Letra no encontrada.';
+  } catch (error) {
+    return 'Error al obtener la letra.';
+  }
+}
