c7660b0d79e24dedc691e6549a88bb286efe0e9a
andrezoficial <andres1997rez@gmail.com>
2025-06-07 17:54:15 -0500

    Prepare deploy to GitHub Pages
    :wq
    git push origin main# Please enter the commit message for your changes. Lines starting
    aq

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import './App.css';
+import { useState } from 'react';
 import SearchBar from './components/SearchBar';
 import SongDetails from './components/SongDetails';
-
-function App() {
-  return (
-    <div className="app-container">
-      <h1>🎵 AndresTunes</h1>
-      <SearchBar />
-      <SongDetails />
-      <footer>
-        <p>© 2025 Andrés Suárez</p>
-        <a href="https://github.com/andrezoficial" target="_blank">GitHub</a> | 
-        <a href="https://www.linkedin.com/in/andres1997rez" target="_blank">LinkedIn</a>
-      </footer>
-    </div>
-  );
-}
-
-export default App;
-import { useState } from 'react';
 import { searchTracks } from './utils/spotify';
 
 function App() {
@@ -31,6 +14,8 @@ function App() {
 
   return (
     <div>
+      <h1>🎵 AndresTunes</h1>
+      <SearchBar />
       <button onClick={handleSearch}>Buscar Shakira</button>
       <ul>
         {tracks.map((track) => (
@@ -40,19 +25,6 @@ function App() {
           </li>
         ))}
       </ul>
-    </div>
-  );
-}
-
-export default App;
-
-import SearchBar from './components/SearchBar';
-
-function App() {
-  return (
-    <div>
-      <h1>🎵 AndresTunes</h1>
-      <SearchBar />
     </div>
   );
 }
