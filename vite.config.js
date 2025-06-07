5ae3f9aa33a5035f66fa9e9e1813473cc23bcdb3
Andres suarez moreno <114126047+andrezoficial@users.noreply.github.com>
2025-06-07 17:10:46 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
+
+export default defineConfig({
+  plugins: [react(), ghPages()],
+  base: '/AndresTunes/' // nombre del repositorio
+});
 
