b62a420f7c2991ef603c23ff604c0d8a08068615
Andres suarez moreno <114126047+andrezoficial@users.noreply.github.com>
2025-06-07 11:01:26 -0500

    Create spotify.js

---
+++ src/utils/spotify.js
@@ -0,0 +1,27 @@
+// src/utils/spotify.js
+
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+
+export async function getAccessToken() {
+  try {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`),
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'grant_type=client_credentials',
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error al obtener el token: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error('Error al obtener el token de Spotify:', error.message);
+    return null;
+  }
+}
