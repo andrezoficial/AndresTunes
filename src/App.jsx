5e70cbcbd27df7ea83ea25d278343dfe46f1279c
Andres suarez moreno <114126047+andrezoficial@users.noreply.github.com>
2025-06-07 11:03:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,28 @@
+import { useState } from 'react';
+import { searchTracks } from './utils/spotify';
+
+function App() {
+  const [tracks, setTracks] = useState([]);
+
+  const handleSearch = async () => {
+    const results = await searchTracks('shakira');
+    setTracks(results);
+  };
+
+  return (
+    <div>
+      <button onClick={handleSearch}>Buscar Shakira</button>
+      <ul>
+        {tracks.map((track) => (
+          <li key={track.id}>
+            {track.name} - {track.artists[0].name}
+            <audio controls src={track.preview_url}></audio>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
 
